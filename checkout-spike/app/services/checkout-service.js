579944b62af58051cf0bb2ce3876b2e3a72de5c0
bilalafzal4014 <bilalafzal4014@gmail.com>
2020-09-11 16:55:32 +0500

    attem_n3d

--- checkout-spike/app/services/checkout-service.js
+++ checkout-spike/app/services/checkout-service.js
@@ -1,11 +1,11 @@
 const ckout = require("../checkout-configurations/checkout-make-connection");
 const axios = require("axios");
 
-class checkoutService{
+class checkoutService {
 
-    getPaymentDetails(cko_session_id){
-
-        return axios.get(`https://api.sandbox.checkout.com/payments/${cko_session_id}`, {
+    getPaymentDetails(cko_session_id) {
+        // both api's are returning the samething
+        let p1 = axios.get(`https://api.sandbox.checkout.com/payments/${cko_session_id}`, {
             headers: {
                 Authorization: process.env.checkoutSecretKey,
                 "Content-Type": "application/json"
@@ -16,9 +16,24 @@ class checkoutService{
             return Promise.reject(error);
         });
 
+        let p2 = ckout.payments.get(cko_session_id)
+            .then(function (response) {
+                return response;
+            }).catch(function (error) {
+                return Promise.reject(error);
+            });
+
+        return Promise.all([p1, p2].map((p) => {
+            return p.catch(e => e)
+        })).then((result) => {
+            return result;
+        }).catch((error) => {
+            return Promise.reject(error);
+        });
+
     }
 
-    makePayment(token){
+    makePayment(token) {
 
         return ckout.payments.request({
             source: {
@@ -30,14 +45,15 @@ class checkoutService{
                 name: 'James Bond',
             },
             "3ds": {
-                "enabled": true
+                enabled: true,
+                attempt_n3d: true //if card is not 3ds enabled then payment flow will not be 3ds and additionally in the response of 3ds enabled card the redirectLink prop will contain 3ds2 inside it
             },
             currency: 'EUR',
             amount: 1000, // cents
             reference: 'ORDER123',
         }).then((response) => {
-                return response;
-            }).catch((error) => {
+            return response;
+        }).catch((error) => {
             return Promise.reject(error);
         });
     }
