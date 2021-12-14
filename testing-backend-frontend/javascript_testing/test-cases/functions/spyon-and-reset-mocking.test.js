13c1c43206dda7361b844063cb89603e01653a3e
Southville <southville@Southvilles-MacBook-Pro.local>
2021-12-14 20:34:30 +0500

    adding more details regarding mocking

--- testing-backend-frontend/javascript_testing/test-cases/functions/spyon-and-reset-mocking.test.js
+++ testing-backend-frontend/javascript_testing/test-cases/functions/spyon-and-reset-mocking.test.js
@@ -107,3 +107,69 @@ test("Is above spyed function available here as well, yes it is 2", () => {
 
 */
 ///////////
+
+
+////////////
+/*
+describe('1', function(){
+    beforeEach(() => {
+        jest.resetAllMocks(); // reset mocks to uninitialized state i.e. undefined
+        jest.restoreAllMocks(); // reset mocks to un-mocked state i.e. original function for spyon case only
+        jest.clearAllMocks(); //to reset the count of called function to zero, remember the case in react where i was inputing on form and expecting api function to not to be called and to be called n times
+    });
+
+    test("mock a function with jest.fn 1", () => {
+        childModuleObject.child = jest.fn().mockImplementation(() => {
+            return "Mocked child function"
+        });
+
+        console.log(parent()); //mocked function
+    });
+
+});
+
+
+
+test("mock a function with jest.fn 2", () => {
+    console.log(parent()); //mocked function
+});
+
+*/
+///////////
+
+
+
+////////////
+/*
+describe('1', function(){
+
+    beforeEach(() => {
+        jest.resetAllMocks(); // reset mocks to uninitialized state i.e. undefined
+        jest.restoreAllMocks(); // reset mocks to un-mocked state i.e. original function for spyon case only
+        jest.clearAllMocks(); //to reset the count of called function to zero, remember the case in react where i was inputing on form and expecting api function to not to be called and to be called n times
+    });
+
+    test("mock a function with jest.fn 1", () => {
+        childModuleObject.child = jest.fn().mockImplementation(() => {
+            return "Mocked child function"
+        });
+
+        console.log(parent()); //mocked function
+    });
+
+    test("mock a function with jest.fn 2", () => {
+        console.log(parent()); //original function
+    });
+
+});
+
+
+test("mock a function with jest.fn 3", () => {
+    console.log(parent()); //original function
+});
+
+*/
+///////////
+
+
+//pattern for above all four are same, basically function get mocked globally and get unmocked also globally. if there is a describe block and jest.restoreAllMocks is there in beforeEach(inside describe block) then describe block should have atleast two test cases and for original/unmocked version to work in test cases outside this describe block
